d44384dac6546bdcc5fb171c2fc3fe16ae45f783
YohAdams <yoh.adams@gmail.com>
2022-08-07 19:29:28 +0200

    add theme provider and theme recoil state

---
+++ src/recoil/theme.ts
@@ -0,0 +1,32 @@
+import type { PaletteMode } from "@mui/material";
+import { atom, useRecoilState, useRecoilValue } from "recoil";
+
+export type ThemeState = { mode: PaletteMode };
+
+export const themeState = atom<ThemeState>({
+  key: "theme",
+  default: { mode: "light" },
+});
+
+export const useThemeState = () => useRecoilValue(themeState);
+
+export const useFlipThemeState = () => {
+  const [theme, setTheme] = useRecoilState(themeState);
+
+  const flip = () => {
+    if (theme.mode === "light") {
+      setTheme({ mode: "dark" });
+      window.localStorage.setItem("theme", "dark");
+    }
+
+    if (theme.mode === "dark") {
+      setTheme({ mode: "light" });
+      window.localStorage.setItem("theme", "light");
+    }
+  };
+
+  return {
+    flip,
+    theme,
+  };
+};
---
+++ src/theme-provider.tsx
@@ -0,0 +1,52 @@
+import type { PaletteMode } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import {
+  createTheme,
+  ThemeProvider,
+} from "@mui/material/styles";
+import React, { FunctionComponent, ReactNode } from "react";
+
+import { useThemeState } from "./recoil/theme";
+
+declare module '@mui/material/styles' {
+    interface Theme {}
+    interface ThemeOptions {}
+  }
+
+const getTheme = (mode: PaletteMode) => ({
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        html {
+          height: 100%;
+        },
+        body {
+          height: 100%
+        },
+        #root {
+          height: 100%;
+        }
+      `,
+    },
+  },
+  palette: {
+    mode,
+  },
+})
+
+export const theme = (mode: PaletteMode) => createTheme(getTheme(mode));
+
+interface AppThemeProviderProps {
+  children: ReactNode;
+}
+
+export const AppThemeProvider: FunctionComponent<AppThemeProviderProps> = ({ children }) => {
+  const themeState = useThemeState();
+  
+  return <ThemeProvider theme={theme(themeState.mode)}>
+      <>
+      <CssBaseline />
+      {children}
+      </>
+    </ThemeProvider>;
+}
